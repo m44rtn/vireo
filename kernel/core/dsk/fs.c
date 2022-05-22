ae148ebc21fe7b97c60547a15cd12778c9c13b78
Maarten Vermeulen <maartenvermeulen70@gmail.com>
2022-05-22 14:16:12 +0200

    fs_api: exposed write for use by the kernel

--- kernel/core/dsk/fs.c
+++ kernel/core/dsk/fs.c
@@ -27,6 +27,9 @@ SOFTWARE.
 #include "diskdefines.h"
 #include "mbr.h"
 
+#include "../memory/memory.h"
+#include "../util/util.h"
+
 #include "../api/api.h"
 #include "../api/syscalls.h"
 
@@ -64,7 +67,7 @@ void fs_api(void *req)
             { fs->hdr.exit_code = EXIT_CODE_GLOBAL_UNSUPPORTED; break;}
             
             // if we get here the device is a known type of hard disk
-            uint16_t drive_part = convert_drive_id(fs->path);
+            uint16_t drive_part = drive_convert_drive_id(fs->path);
             fs->hdr.response = mbr_get_type( (uint8_t) (drive_part >> 8), (uint8_t) (drive_part & 0xFF));
             break;
         }
@@ -94,12 +97,13 @@ void fs_api(void *req)
             { fs->hdr.exit_code = EXIT_CODE_GLOBAL_UNSUPPORTED; break; }
 
             if(fs->f == NULL)
+            {fs->hdr.exit_code = EXIT_CODE_GLOBAL_OUT_OF_RANGE; break;}
 
             drv[0] = FS_COMMAND_WRITE;
             drv[1] = (uint32_t) fs->path;
             drv[2] = (uint32_t) fs->f;
             drv[3] = (uint32_t) fs->size;
-            drv[4] = FAT_FILE_ATTRIB_FILE;
+            drv[4] = FAT_FILE_ATTRIB_FILE; // FIXME, should be configured by the user...
             driver_exec_int(DRIVER_TYPE_FS | driver_type, &drv[0]);
             
             fs->hdr.exit_code = (uint8_t) drv[4];
@@ -163,4 +167,18 @@ file_t *fs_read_file(char *fpath, size_t *o_size)
     *o_size = req.hdr.response_size;
     
     return req.hdr.response_ptr;
+}
+
+err_t fs_write_file(char *fpath, file_t *file, size_t fsize)
+{
+    fs_t req = {
+        .hdr.system_call = SYSCALL_FS_WRITE,
+        .path = fpath,
+        .f = file,
+        .size = fsize,
+    };
+
+    fs_api(&req);
+    
+    return req.hdr.exit_code;
 }
--- kernel/core/dsk/fs.h
+++ kernel/core/dsk/fs.h
@@ -29,4 +29,6 @@ SOFTWARE.
 void fs_api(void *req);
 file_t *fs_read_file(char *fpath, size_t *o_size);
 
+err_t fs_write_file(char *fpath, file_t *file, size_t fsize);
+
 #endif
