6aa3a181d350c24f3c8d484276b07fdd37603e0d
Maarten Vermeulen <maartenvermeulen70@gmail.com>
2022-12-18 00:15:25 +0100

    fix wrong return size of elf_calc_size_in_memory()

--- kernel/core/exec/elf.c
+++ kernel/core/exec/elf.c
@@ -131,12 +131,11 @@ static size_t elf_calc_size_in_memory(const elf_program_t *prog, const uint32_t 
             continue;
 
         last_paddr = prog[i].paddr;
-        
         last_memsize = prog[i].memsize;
-        s = s + last_memsize;
     }
 
     *(npages) = ((last_paddr + last_memsize) / PAGE_SIZE) + (last_memsize % PAGE_SIZE != 0);
+    s = last_paddr + last_memsize;
 
     return s;
 }
