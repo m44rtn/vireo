705ee2def3a1ad4c211cd8d69e0cc56c265d0663
Maarten Vermeulen <maartenvermeulen70@gmail.com>
2022-12-26 23:06:36 +0100

    screen_basic: do not \b when at start of screen

--- kernel/core/screen/screen_basic.c
+++ kernel/core/screen/screen_basic.c
@@ -340,7 +340,8 @@ static void screen_basic_char_put_on_screen(char c){
 
 	switch(c){
 			case ('\b'):
-				SCRscreenData.cursorX--;
+				if(SCRscreenData.cursorX > 0)
+					SCRscreenData.cursorX--;
 				vidmem[(SCRscreenData.cursorY * SCREEN_BASIC_WIDTH + SCRscreenData.cursorX)*SCREEN_BASIC_DEPTH] = 0;  
 			
 			break;
