f274dbda76dff45e956f63eba38713d6b71809d9
Maarten Vermeulen <maartenvermeulen70@gmail.com>
2023-11-03 01:10:18 +0100

    Update version info

--- kernel/core/kernel/info.h
+++ kernel/core/kernel/info.h
@@ -26,7 +26,7 @@ SOFTWARE.
 
 /* this is always the build number for next build. 
 current binary build = [BUILD] - 1 */
-#define BUILD 5708
+#define BUILD 5976
 
 #define MAJOR 0
 #define MINOR 2  
